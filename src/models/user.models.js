6b708b8c28718c58568b6c58b583cb0fe1122ed2
OmWadhwani <121439268+OmWadhwani@users.noreply.github.com>
2025-07-17 19:24:46 +0530

    update modified .

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
+import  jwt  from "jsonwebtoken";
 const userschema = new Schema({
   Uername: {
     type: String,
@@ -45,5 +46,31 @@ type: Schema.types.ObjectId, ref:"video"
   }
 },
 {timestamps:true});
+
+userschema.pre("save", async function(next){
+  // password encryption
+  if(!this.modified("password")) return next()
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+})
+
+userschema.methods.isPasswordcorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN, // Secret key
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY, // Expiry duration
+    }
+  );
+};
+
 
 export const User = mongoose.model('User', userschema);
